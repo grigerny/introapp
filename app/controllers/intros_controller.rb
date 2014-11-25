ad0f00791fa47e69178128a1cf9c31dbbae8010c
Gary Riger <grigerny@aol.com>
2014-11-25 18:48:10 -0500

    added before action authentication

--- app/controllers/intros_controller.rb
+++ app/controllers/intros_controller.rb
@@ -1,5 +1,6 @@
 class IntrosController < ApplicationController
   before_action :set_intro, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, :except => [:index]
 
   # GET /intros
   # GET /intros.json

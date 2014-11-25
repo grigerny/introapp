faa1e273aa25c61259fbbcf080d31c58e8e21ea7
Gary Riger <grigerny@aol.com>
2014-11-25 15:36:09 -0500

    added a sample email

--- app/models/intro.rb
+++ app/models/intro.rb
@@ -1,3 +1,5 @@
 class Intro < ActiveRecord::Base
   attr_accessible :person1_name, :person1_email, :person2_name, :person2_email
+  
+  validates_presence_of :person1_name, :person1_email, :person2_name, :person2_email
 end

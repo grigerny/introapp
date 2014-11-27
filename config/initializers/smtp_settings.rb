fdcda969a01369255f2217bc7db1fdf584454a6f
Gary Riger <grigerny@aol.com>
2014-11-27 00:21:44 -0500

    updated smtp

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
     :enable_starttls_auto => true,
-    :address        => 'gator2013.hostgator.com',
-    :port           => 465,
-    :domain         => 'yointro.com',
+    :address        => 'smtp.gmail.com',
+    :port           => 587,
+    :domain         => 'gmail.com',
     :authentication => :plain,
-    :user_name      => 'intro@yointro.com',
-    :password       => 'elwin11'
+    :user_name      => 'yointroapp@gmail.com',
+    :password       => 'yointroapp123'
   }

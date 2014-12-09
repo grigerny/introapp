e32d2249f1161dd7226be37bbd703a82632be5dc
Gary Riger <grigerny@aol.com>
2014-12-09 16:13:18 -0500

    updated

--- config/initializers/omnicontacts.rb
+++
@@ -1,5 +0,0 @@
-require "omnicontacts"
-
-Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, "330061137178-0bhe1iu4dlmsrvu886iadpjkmkgvqbpd.apps.googleusercontent.com", "OkqpLYVyQ7E1y4dk_VYe3q5t", {:redirect_path => "/users/oauth2callback", :ssl_ca_path => "/etc/ssl/certs/curl-ca-bundle.crt"}
-  end

ecc7ce4eaebc60ea7dc73637797cd25137268f65
Gary Riger <grigerny@aol.com>
2014-12-09 20:00:16 -0500

    updated bcc

--- app/mailers/intro_mailer.rb
+++ app/mailers/intro_mailer.rb
@@ -3,7 +3,7 @@ class IntroMailer < ActionMailer::Base
   def intro_email(intro, login_user)
     @intro = intro
     @user = @intro.user
-    mail(from: login_user.email, to: @intro.person1_email + "," + @intro.person2_email, subject: @intro.person1_name + "," + ' meet ' + @intro.person2_name, reply_to: @intro.user.email)
+    mail(from: login_user.email, to: @intro.person1_email + "," + @intro.person2_email, subject: @intro.person1_name + "," + ' meet ' + @intro.person2_name, reply_to: @intro.user.email, bcc: login_user.email)
   end
 
 end
